d00524a7e6987bbb7686bf7dc33d89ead6030d04
Eliot <eliot.meurillon@gmail.com>
2022-04-26 17:05:47 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const App = () => {
+
+	return (
+		<div class="bg-neutral-800 h-screen flex items-center justify-center">
+      <div class="w-96 h-96 bg-red-600 rounded-full absolute overflow-visible"></div>
+  <div class="bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-5">
+	<div class=" text-center">
+		<div class="text-7xl text-blue-300 leading-5">”</div>
+		<div class="font-medium text-white max-w-xl text-xl">
+			We know the ecosystem very well and they provides the speed, flexible account model and API-first approach
+			that no one else can. These aspects are essential to building certain products that power our customers'
+			growth.
+		</div>
+		<div class="mt-5">
+			<span class="font-bold">Henrique Dubugras</span><span class="text-gray-500 font-medium"> — Co-Founder and CEO, Acme</span>
+		</div>
+	</div>
+</div>
+</div>
+		)
+}
+
+export default App
